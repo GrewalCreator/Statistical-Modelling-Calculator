86f9b897d6714b44a6a9c477e2f6cb9f88172e0c
GrewalCreator <gurtejgrewal455@gmail.com>
2022-10-07 12:44:15 -0400

    Implemented quartile and median calculations

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -1,18 +1,14 @@
 import math
 
-data: list[float]
-
-data = [2.71, 2.81, 2.81, 2.84, 2.84, 2.85, 2.86, 2.94, 2.95, 2.96, 2.97, 2.98, 2.98, 2.99, 3.03, 3.05, 3.05, 3.06,
-        3.09, 3.11]
-
 
 class Calculate(object):
 
-    def __init__(self):
+    def __init__(self, data):
         self.dataList = data
         self.data_length = len(self.dataList)
+        self.quartile_data: list[float] = []
 
-    def calculate_mean(self):
+    def calculate_mean(self) -> float:
         local_mean: float = 0
         for i in range(0, self.data_length):
             local_mean += self.dataList[i]
@@ -20,7 +16,7 @@ class Calculate(object):
         return local_mean
 
     @property
-    def get_mean(self):
+    def get_mean(self) -> float:
         return self.calculate_mean()
 
     def calculate_standard_deviation(self) -> float:
@@ -32,11 +28,57 @@ class Calculate(object):
         return local_std_variation
 
     @property
-    def get_std_variation(self):
+    def get_std_variation(self) -> float:
         return self.calculate_standard_deviation()
 
+    def calculate_quartile(self) -> list[float]:
 
-calc = Calculate()
-y = calc.get_mean
-x = calc.get_std_variation
-print(x)
+        list_first_half = self.get_sorted_data[0:int(len(self.get_sorted_data) / 2)]
+        list_second_half = self.get_sorted_data[int(len(self.get_sorted_data)/2): len(self.get_sorted_data)]
+
+        midQ: float = self.calculate_median(self.get_sorted_data)
+        lowerQ: float = self.calculate_median(list_first_half)
+        upperQ: float = self.calculate_median(list_second_half)
+
+        self.quartile_data.append(lowerQ)
+        self.quartile_data.append(midQ)
+        self.quartile_data.append(upperQ)
+
+        return self.quartile_data
+
+    @property
+    def get_all_quartiles(self) -> list[float]:
+        return self.calculate_quartile()
+
+    @staticmethod
+    def calculate_median(local_list: list[float]) -> float:
+        median: float = -1
+        local_list_length = len(local_list)
+
+        if local_list_length % 2 == 0:
+            median = (local_list[int(local_list_length / 2)] + local_list[int((local_list_length / 2)) + 1]) / 2
+        elif local_list_length % 2 == 1:
+            median = (local_list_length + 1) / 2
+
+        return median
+
+    def get_median(self):
+        return self.calculate_median(self.get_sorted_data)
+
+    @property
+    def get_sorted_data(self):
+        return sorted(self.dataList)
+
+
+def main():
+    data: list[float] = [2.71, 2.81, 2.81, 2.84, 2.84, 2.85, 2.86, 2.94, 2.95, 2.96, 2.97, 2.98, 2.98, 2.99, 3.03,
+                         3.05, 3.05, 3.06, 3.09, 3.11]
+
+    calc = Calculate(data)
+
+    print(calc.get_all_quartiles)
+    print(calc.get_median())
+
+
+if __name__ == "__main__":
+    main()
